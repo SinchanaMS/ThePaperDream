e33592091c6892d0b65a43fcde64a6734695af19
SinchanaMS <94738532+SinchanaMS@users.noreply.github.com>
2022-03-24 13:15:39 +0530

    PR Changes: convert categories, carousel to array

--- src/components/LandingPage/Carousel/Carousel.jsx
+++ src/components/LandingPage/Carousel/Carousel.jsx
@@ -1,55 +1,29 @@
 import { Link } from "react-router-dom";
 import "./Carousel.css"
+import { carousel } from "../../../backend/db/carousel";
 
 export default function Carousel(){
    
     return (
         <section className="carousel-base grid-carousel carousel-slides">
-        <div className="slide">
-            <div className="carousel-child carousel-img"><img
-                    src={process.env.PUBLIC_URL + 'assets/Carousel_Images/carousel.png'}
-                    alt="Planners-Pastel-Collection" className="img"/></div>
-            <div className="carousel-child grid-child grid-text">
-                <h1>Planners - Pastel Collection</h1>
-                <h3>Starting at Rs. 399</h3>
-                <button className="pri-btn">
-                   <Link to="/products">Shop Now</Link>
-                </button>
-                <button className="sec-btn">
-                    <Link to="/wishlist" className="link-in-btn">Wishlist</Link>
-                </button>
-            </div>
-        </div>
-        <div className="slide">
-            <div className="carousel-child carousel-img"><img
-                    src={process.env.PUBLIC_URL + 'assets/Carousel_Images/carousel2.png'}
-                    alt="Washi-Tapes-Celestial-Collection" className="img"/></div>
-            <div className="carousel-child grid-child grid-text">
-                <h1>Washi Tapes - Celestial Collection</h1>
-                <h3>Starting at Rs. 699</h3>
-                <button className="pri-btn">
-                    <Link to="/products">Shop Now</Link>
-                </button>
-                <button className="sec-btn">
-                    <Link to="/wishlist" className="link-in-btn">Wishlist</Link>
-                </button>
-            </div>
-        </div>
-        <div className="slide">
-            <div className="carousel-child carousel-img"><img
-                    src={process.env.PUBLIC_URL + 'assets/Carousel_Images/carousel3.png'}
-                    alt="Zodiac-Collection" className="img"/></div>
-            <div className="carousel-child grid-child grid-text">
-                <h1>ZODIAC SERIES</h1>
-                <h3>Planners and Journals with Zodiac Sign Covers</h3>
-                <button className="pri-btn">
-                    <Link to="/products">Shop Now</Link>
-                </button>
-                <button className="sec-btn">
-                    <Link to="/wishlist" className="link-in-btn">Wishlist</Link>
-                </button>
-            </div>
-        </div>
+            {carousel.map(({carouselName, carouselSubtitle, carouselImg})=>
+            (
+                <div className="slide" key={carouselName}>
+                    <div className="carousel-child carousel-img">
+                        <img src={carouselImg} alt={carouselName} className="img"/>
+                    </div>
+                    <div className="carousel-child grid-child grid-text">
+                        <h1>{carouselName}</h1>
+                        <h2>{carouselSubtitle}</h2>
+                        <button className="pri-btn">
+                            <Link to="/products">Shop Now</Link>
+                        </button>
+                        <button className="sec-btn">
+                            <Link to="/wishlist" className="link-in-btn">Wishlist</Link>
+                        </button>
+                    </div>
+            </div>  
+            ))}
     </section>
     )
 }
