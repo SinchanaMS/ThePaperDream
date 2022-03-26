a2c4cdbdf02bff9852b0ed9a01044c3b2cb80c08
SinchanaMS <94738532+SinchanaMS@users.noreply.github.com>
2022-03-26 16:26:15 +0530

    refactored: fetch products from db

--- src/pages/ProductListing/ProductsList.jsx
+++ src/pages/ProductListing/ProductsList.jsx
@@ -1,14 +1,25 @@
-import {products} from "../../backend/db/products"
 import { Card, FilterPane } from "../../components/components"
+import {useState, useEffect} from 'react'
+import axios from "axios"
 import "./productList.css"
 
 export default function ProductsList() {
+  const [prodList, setProdList] = useState([])
+  
+  useEffect(() => {
+    const fetchProducts = async () =>{
+      const response = await axios.get('/api/products') 
+      setProdList(response.data.products)
+    };
+    fetchProducts()
+  },[])
+
   return (
     <div className=" ">
       <section className="prod-listing prod-child products-container">
         <FilterPane/>
         <div className="products-list">
-          {products.map(product => (
+          {prodList.map(product => (
             <Card products={product} key={product._id}/>
           ))}
         </div>
