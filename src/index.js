83acd302cbeba1ad002544a1bdb97b89ca6d9aec
SinchanaMS <94738532+SinchanaMS@users.noreply.github.com>
2022-03-31 22:28:37 +0530

    feat5: Added Auth Pages and Functionality

--- src/index.js
+++ src/index.js
@@ -4,17 +4,20 @@ import App from "./App";
 import { makeServer } from "./server";
 import { BrowserRouter } from "react-router-dom";
 import { FilterProvider } from "./contexts/FilterContext";
+import { AuthProvider } from "./contexts/AuthContext";
 
 // Call make Server
 makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
-    <FilterProvider>
     <BrowserRouter>
+    <AuthProvider>
+    <FilterProvider>
     <App />
-    </BrowserRouter>
     </FilterProvider>
+    </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
